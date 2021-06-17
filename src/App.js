4b14ead821120cd1a81fe3538326b094b429967b
iamshin <xinlong95@hotmail.com>
2021-06-17 11:10:10 +0800

    Adding accordion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {JumbotronContainer} from './containers/jumbotron';
 import {FooterContainer} from './containers/footerContainer'
+import {FaqsContainer} from './containers/faq'
 function App() {
   return (
     <>
       <JumbotronContainer/>
+      <FaqsContainer/>
       <FooterContainer/>
     </>
   );
