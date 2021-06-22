6834e2f78867033344c00603180c9d3c018d7525
iamshin <xinlong95@hotmail.com>
2021-06-23 00:14:23 +0800

    added router

--- src/App.js
+++ src/App.js
@@ -2,20 +2,37 @@ import React from 'react';
 import Home from './pages/home'
 import SignIn from './pages/signin'
 import Signup from './pages/signup'
+import Browse from './pages/browse'
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes'
+import * as ROUTES from './constant/routes'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
+  const user = null
   return (
     <Router>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/signin">
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_IN}
+        exact
+      >
         <SignIn />
-      </Route>
-      <Route exact path="/signup">
+      </IsUserRedirect>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_UP}
+        exact
+      >
         <Signup />
-      </Route>
+      </IsUserRedirect>
+      <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+        <Browse />
+      </ProtectedRoute>
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
+        <Home />
+      </IsUserRedirect>
     </Router>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {HeaderContainer,JumbotronContainer,PlansContainer,FaqsContainer,FooterContainer} from '../containers'
+import { HeaderContainer, JumbotronContainer, PlansContainer, FaqsContainer, FooterContainer } from '../containers'
 
-export default function Home(){
-    return(
-        <>
-          <HeaderContainer/>
-          <JumbotronContainer/>
-          <PlansContainer/>
-          <FaqsContainer/>
-          <FooterContainer/>
-        </>
-    )
+export default function Home() {
+  return (
+    <>
+      <HeaderContainer />
+      <JumbotronContainer />
+      <PlansContainer />
+      <FaqsContainer />
+      <FooterContainer />
+    </>
+  )
 }
